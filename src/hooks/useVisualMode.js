bcf0c8a2cd41cef64a101059a9a660e1b1a7bfca
samsam3268 <samskleung@gmail.com>
2022-01-27 02:16:23 -0800

    remove console.logs

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,38 +1,34 @@
 import { useState } from "react";
 
 export default function useVisualMode(initialMode) {
+
   const [mode, setMode] = useState(initialMode);
+  //history is to track previous modes
   const [history, setHistory] = useState([initialMode]);
-  //console.log('<-----start run----->')
-  //console.log('mode : ', mode)
+
   function transition(newMode, replace = false) {
-    
     if (replace) { // if replace is true
-      const newHistory = [...history]
-      //console.log('newHistory', newHistory)
-      newHistory.pop()
-      setMode(newMode)
-      setHistory([...newHistory, newMode])
-      console.log('History1', history)
-    } else { // if replace is false   
-      setMode(newMode)
-      setHistory([...history, newMode])
-      console.log('History2', history)
+      const newHistory = [...history];
+      newHistory.pop();
+      setMode(newMode);
+      setHistory([...newHistory, newMode]);
+    } else { // if replace is false
+      setMode(newMode);
+      setHistory([...history, newMode]);
     }
   }
 
   function back() {
-    const newHistory = [...history]
+    const newHistory = [...history];
+    //make sure user can only go back to the inital mode
     if (newHistory.length > 1) {
-      newHistory.pop()
-    }
-    setMode(newHistory.slice(-1)[0])
-    setHistory(newHistory)
-    console.log('History3', history)
+      newHistory.pop();
+    };
+
+    setMode(newHistory.slice(-1)[0]);
+    setHistory(newHistory);
   }
 
-  //console.log('OutsideHistory ', history)
-  //console.log('<-----end run----->')
   return { mode, transition, back, history };
 }
 
